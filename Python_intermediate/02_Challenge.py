2433532c74ad3bd19a51826cd835e2b4695d1545
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-04-15 05:46:29 -0400

    Refactor Fizz Buzz and Anagram functions; implement Fibonacci sequence with multiple methods

--- Python_intermediate/02_Challenge.py
+++ Python_intermediate/02_Challenge.py
@@ -2,6 +2,7 @@
 
 print("\n")
 
+                                                    # EL FAMOSO "FIZZ BUZZ"
 """
 Escribe un programa que muestre por consola (con un print) los
 números de 1 a 100 (ambos incluidos y con un salto de línea entre
@@ -29,9 +30,11 @@ numero() # Llamo a la funcion numero().
 
 print("\n")
 
+
 """
 
-Es un Anagrama?
+                                                        Es un Anagrama?
+
 Escribe una función que reciba dos palabras (String) y retorne
 verdadero o falso (Bool) según sean o no anagramas.
 
@@ -41,14 +44,85 @@ las letras de otra palabra inicial.
 - Dos palabras exactamente iguales no son anagrama. """
 
 def anagrama (palabra1, palabra2): # Defino la funcion anagrama que recibe dos palabras.
+    
     if palabra1.lower() == palabra2.lower():
         return False
-    else:
-        return True
+    
+    # Le pongo el metodo sorted() para que me ordene las letras de las palabras y las compare.
+    # Le pongo el metodo lower() para que me convierta las letras a minusculas y las compare.
+    return sorted(palabra1.lower()) == sorted(palabra2.lower()) # Ordeno las letras de las palabras y comparo si son iguales.
     
 print(anagrama("roma", "Amor")) # Llamo a la funcion anagrama y le paso las palabras "roma" y "Amor".
 
 
+print("\n")
 
 
+                                                # LA SUCESIÓN DE FIBONACCI
+
+"""
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+"""
+
+def fibonacci():
+    
+    n = 50 # Limite de numeros a iterar.
+    contador = 0 # Inicializo el contador en 0.
+    first = 0 # Inicializo la variable a en 0.
+    next = 1 # Inicializo la variable b en 1.
+    
+    while contador < n: # Hago un bucle while que se repite 50 veces.
+        print(first, end=", ") # Imprimo el valor de a y le pongo una coma al final.
+
+        total = first + next # Sumo a y b y lo guardo en la variable total.
+        first = next 
+        next = total
+        contador += 1 # Incremento el contador en 1.
+
+fibonacci() # Llamo a la funcion fibonacci().
+
+
+
+print("\n")
+
+# Otra forma de hacerlo es con una lista vacia y un for loop.
+def Fibonacci():
+    
+    fib = [] # Inicializo una lista vacia.
+    
+    for i in range(0,50): # Hago un for loop de 0 a 50.
+        
+        if i == 0: # Si i es 0, entonces agrego 0 a la lista.
+            fib.append(0) # Agrego 0 a la lista.
+        elif i == 1: # Si i es 1, entonces agrego 1 a la lista.
+            fib.append(1) # Agrego 1 a la lista.
+
+        else: # Si i es mayor a 1, entonces agrego la suma de los dos anteriores a la lista.
+            fib.append(fib[i-1] + fib[i-2]) # Agrego la suma de los dos anteriores a la lista.
+        
+    return fib # Retorno la lista.
+
+print(Fibonacci())# Llamo a la funcion fibonacci().
+
+print("\n")
+
+
+# Otra Forma para la realizar la Sucesion de Fibonacci con el Bucle for.
+def FIBONACCI():
+
+    prev = 0 # Inicializo la variable a en 0.
+    next = 1 # Inicializo la variable b en 1.
+
+    for index in range(0,50):
+        print(prev, end=", ") # Imprimo el valor de a y le pongo una coma al final.
+
+        prev, next = next, prev + next
+        
+FIBONACCI() # Llamo a la funcion fibonacci().
+
+print("\n")
 
