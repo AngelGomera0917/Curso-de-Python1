4c244a8e0dcefd386df44d00e29ea5c03cc1b0a7
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-05-18 22:50:05 -0400

    No code changes detected in test_funciones.py

--- tests/test_funciones.py
+++ tests/test_funciones.py
@@ -6,17 +6,24 @@ from Python_Basic.Functions09 import (
     recomendacion_planta
 )
 
-def tes_sum_two_values_with_return():
+def test_sum_two_values_with_return():
     assert sum_two_values_with_return(3, 4) == 7
     assert sum_two_values_with_return(-1, 1) == 0
     assert sum_two_values_with_return(0, 0) == 0
     
 
-def test_verificar():
+def test_verificar_positivo():
     assert verificar (8) == 8
-    assert verificar (-5) == None
     assert verificar (6) == 6
-    assert verificar (-10) == None
+    assert verificar (0) == 0
+
+    
+def test_verificar_negativo():
+    assert verificar (-5) is None
+    assert verificar (-10) is None
+    assert verificar (-1) is None
+    assert verificar (-100) is None
+
 
 def test_resta():
     assert resta(8, 3) == 5
@@ -28,6 +35,4 @@ def test_recomendacion_planta():
     assert recomendacion_planta("alto") == "Orquídea"
     assert recomendacion_planta("medio") == "Suculenta"
     assert recomendacion_planta("Full") == "No se encontró ninguna planta"
-    
 
-    
