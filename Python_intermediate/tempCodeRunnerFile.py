2daf176cddf53bfde5f2487a6dfa1fed30942cd7
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-06-01 00:31:36 -0400

    Remove unnecessary comments and clean up Fibonacci sequence logic

--- Python_intermediate/tempCodeRunnerFile.py
+++ Python_intermediate/tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-else: # Si i es mayor a 1, entonces agrego la suma de los dos anteriores a la lista.
-            fib.append(fib[i-1] + fib[i-2]) # Agrego la suma de los dos anteriores a la lista.
+8, 10, 14, 5, 17, 19, 20, 24, 23
