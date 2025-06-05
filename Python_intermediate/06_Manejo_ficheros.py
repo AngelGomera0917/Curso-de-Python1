2d1bce60947fb0cf9369b31287dbcf7563ee8cb9
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-06-04 20:02:39 -0400

    Refactor file handling in 06_Manejo_ficheros.py for improved readability

--- Python_intermediate/06_Manejo_ficheros.py
+++ Python_intermediate/06_Manejo_ficheros.py
@@ -1,6 +1,8 @@
 
 # ========================= Manejo de Ficheros ========================= #
 
+# file .txt
+
 import os # Módulo para interactuar con el sistema operativo
 # El módulo os se importa para interactuar con el sistema operativo desde Python.
 # Permite realizar tareas como:
@@ -13,15 +15,15 @@ import os # Módulo para interactuar con el sistema operativo
 buffer = open("fichero.txt", "w+")  # Abrir un fichero en modo escritura
 buffer.write("Hola, este es un fichero de texto de Angel and Gomera...\n")
 buffer.write("Mi Lenguaje preferido es Python\n")
-buffer.seek(0)  # Mover el cursor al inicio del fichero, para que pueda leer desde el principio
+# buffer.seek(0)  # Mover el cursor al inicio del fichero, para que pueda leer desde el principio
 print(buffer.read(28)) # Imprime el contenido del fichero
 buffer.close() # Cierra el buffer
 
 # Leer el archivo 
 buffer = open("fichero.txt", "r+")
 bu = buffer.readline() # Lee la primera línea del fichero
-print(bu)
-print(buffer.read())  # Imprime el resto del contenido del fichero
+# print(bu)
+# print(buffer.read())  # Imprime el resto del contenido del fichero
 # buffer.close() # Cierra el buffer
 
 # Añadir contenido al archivo
@@ -29,7 +31,7 @@ buffer.write("Mi color favorito es Black.\n")  # Añade una nueva línea al
 buffer.write("Soy un excelente estudiante.\n")  # Añade una nueva línea al 
 buffer.write("Amo la programacion\n")  # Añade una nueva línea al 
 buffer.write("Mi objetivo es ser un desarrollador Full-Stack.\n")  # Añade una nueva línea al 
-buffer.seek(0) # Mueve el cursor al inicio del fichero
+# buffer.seek(0) # Mueve el cursor al inicio del fichero
 
 for contenido_lineas in buffer.readlines():  # La lista que me pasa el readlines(), la vuelvo iterable con el bucle for
     print(contenido_lineas)  # Imprime cada línea del fichero
@@ -38,6 +40,31 @@ for contenido_lineas in buffer.readlines():  # La lista que me pasa el readlines
 # readline() # Devuelve una línea del fichero.
 
 # os.remove("fichero.txt")  # Elimina el fichero creado.
+
+
+# file .json
+
+import json  # Importa el módulo json para trabajar con archivos JSON
+
+# Crear un diccionario de ejemplo
+my_json = {
+    "nombre": "Angel",
+    "edad": 25,
+    "Casa": {
+        "Piso": 1,
+        "Apartamento": 201,
+        "Dirección": "Calle 123, Numero 456"},
+    "Correo": "antoniogomera12@gmail.com",
+    "lenguajes_preferidos": ["Python", "JavaScript", "C#", "Kotlin"],
+    "colores_favoritos": ["Black", "Blue", "Red"]
+}
+
+json_file = open("fichero.json", "w")  # Abre un fichero JSON en modo escritura
+
+json.dump(my_json, json_file, indent = 4) # Dump es para escribir el diccionario en el fichero JSON
+# indent = 4 es para que el JSON se vea más bonito, con una sangría de 4 espacios.
+
+json_file.close()
 
 
 
