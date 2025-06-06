75ee9a4e2ec335fed9a7cbcd9b4d812a8aed7eac
Angel-Gomera <20220493@itla.edu.do>
2025-06-06 17:01:29 -0400

    Refactor file handling to improve readability and add output separation

--- Python_intermediate/06_Manejo_ficheros.py
+++ Python_intermediate/06_Manejo_ficheros.py
@@ -15,26 +15,32 @@ import os # Módulo para interactuar con el sistema operativo
 buffer = open("fichero.txt", "w+")  # Abrir un fichero en modo escritura
 buffer.write("Hola, este es un fichero de texto de Angel and Gomera...\n")
 buffer.write("Mi Lenguaje preferido es Python\n")
-# buffer.seek(0)  # Mover el cursor al inicio del fichero, para que pueda leer desde el principio
+buffer.seek(0)  # Mover el cursor al inicio del fichero, para que pueda leer desde el principio
 print(buffer.read(28)) # Imprime el contenido del fichero
 buffer.close() # Cierra el buffer
+
+print("\n")  # Imprime una línea en blanco para separar la salida
 
 # Leer el archivo 
 buffer = open("fichero.txt", "r+")
 bu = buffer.readline() # Lee la primera línea del fichero
-# print(bu)
-# print(buffer.read())  # Imprime el resto del contenido del fichero
-# buffer.close() # Cierra el buffer
+print(bu)
+print(buffer.read())  # Imprime el resto del contenido del fichero
+buffer.close() # Cierra el buffer
+
+print("\n")  # Imprime una línea en blanco para separar la salida
 
 # Añadir contenido al archivo
 buffer.write("Mi color favorito es Black.\n")  # Añade una nueva línea al 
 buffer.write("Soy un excelente estudiante.\n")  # Añade una nueva línea al 
 buffer.write("Amo la programacion\n")  # Añade una nueva línea al 
 buffer.write("Mi objetivo es ser un desarrollador Full-Stack.\n")  # Añade una nueva línea al 
-# buffer.seek(0) # Mueve el cursor al inicio del fichero
+buffer.seek(0) # Mueve el cursor al inicio del fichero
 
 for contenido_lineas in buffer.readlines():  # La lista que me pasa el readlines(), la vuelvo iterable con el bucle for
     print(contenido_lineas)  # Imprime cada línea del fichero
+
+print("\n")  # Imprime una línea en blanco para separar la salida
 
 # readlines() # Devuelve una lista con todas las líneas del fichero.
 # readline() # Devuelve una línea del fichero.
