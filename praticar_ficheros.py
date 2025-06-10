9c13a046dc896ceb1df1871398acc796a61b00e9
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-06-10 01:04:10 -0400

    Add CSV file handling to store and read product data

--- praticar_ficheros.py
+++ praticar_ficheros.py
@@ -1,7 +1,33 @@
 
 # Praticar Ficheros .txt
-with open("nota.txt", "w") as escribir:
-    escribir.write("Darli es Hermosa")
-    escribir.write("Amo la programacion")
-    escribir.write("Mi pelis preferida es Fast and furious")
-    escribir.write("Mi lenguaje favorito es Python")
+
+import csv
+
+computer = {
+    "producto_1": {
+        "Nombre": "Teclado",
+        "Precio": 1235},  
+    
+    "producto_2": {
+        "Nombre": "Mouse",
+        "Precio": 900},   
+    
+}
+
+computer["producto_3"] = {
+    "Nombre": "Monitor",
+    "Precio": 12800 }
+
+with open("nota.csv", "w") as file_csv:
+    guardar_csv = csv.writer(file_csv)
+    
+    guardar_csv.writerow(["Producto", "Precio", "Cantidad"])
+    guardar_csv.writerow(["Teclado", 1235, 2])
+    guardar_csv.writerow(["Mouse", 980, 1])
+    guardar_csv.writerow(["Monitor", 14850, 2])
+
+with open("nota.csv", "r") as reader_csv:
+    imprimir_csv = csv.reader(reader_csv)
+
+    for index in reader_csv:
+        print(index)
