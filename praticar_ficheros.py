3db570416e2f3db26167b53f0db5815344bd6c98
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-06-09 23:38:42 -0400

    Add file handling example to create nota.txt with personal preferences

---
+++ praticar_ficheros.py
@@ -0,0 +1,7 @@
+
+# Praticar Ficheros .txt
+with open("nota.txt", "w") as escribir:
+    escribir.write("Darli es Hermosa")
+    escribir.write("Amo la programacion")
+    escribir.write("Mi pelis preferida es Fast and furious")
+    escribir.write("Mi lenguaje favorito es Python")
