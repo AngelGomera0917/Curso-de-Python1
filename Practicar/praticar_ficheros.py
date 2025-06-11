3f7cd19b4f75f9048b09e9d468979aa1d37c6993
Angel-Gomera <20220493@itla.edu.do>
2025-06-11 10:47:50 -0400

    Update file paths in practicar_ficheros.py to use the 'practicar' directory

--- Practicar/praticar_ficheros.py
+++ Practicar/praticar_ficheros.py
@@ -3,14 +3,14 @@
 import csv
 
 
-with open ("venta.csv", "w") as file_csv:
+with open ("practicar/venta.csv", "w") as file_csv:
     escribir = csv.writer(file_csv)
     escribir.writerow(["Producto", "Precio", "Cantidad"])
     escribir.writerow(["Teclado", 1235, 5])
     escribir.writerow(["Mouse", 980, 7])
     escribir.writerow(["Monitor", 7640, 2])
 
-with open ("venta.csv", "r") as file_reader: 
+with open ("practicar/venta.csv", "r") as file_reader: 
     leer = csv.reader(file_reader)
     
     next(leer)
@@ -25,7 +25,7 @@ with open ("venta.csv", "r") as file_reader:
     print("\n")
 
 
-with open ("tareas.txt", "r+") as file_read:
+with open ("practicar/tareas.txt", "r+") as file_read:
     file_read.seek(16)
     print(file_read.read()) # 
     
