8563b9c79347a114ba8d7530591d8f942e4913f0
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-06-25 23:45:46 -0400

    Refactor code structure in 08_Manejo_paquetes.py for clarity and organization

--- Python_intermediate/08_Manejo_paquetes.py
+++ Python_intermediate/08_Manejo_paquetes.py
@@ -2,3 +2,10 @@
 # Manejo de Paquetes 
 
 # Un paquete es un conjunto de módulos que se agrupan para facilitar su uso y organización.
+
+import numpy # importación del paquete numpy
+
+array_nu = numpy.array([3, 5, 6, 8,1 ,7 ,2 ,9]) # Uso array para crear una lista de los 
+
+print(array_nu + 4)
+
