ffbbcb35920bff7c83f4997f809cb4507aef7028
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-07-03 22:23:12 -0400

    Refactor decorator implementation for clarity and maintainability

--- Backend/decoradores.py
+++ Backend/decoradores.py
@@ -4,16 +4,20 @@
 
 # Un decorador es una función que recibe otra función como argumento y devuelve una nueva función que generalmente extiende o modifica el comportamiento de la función original.
 
-def my_decorador(func):
-    def show_info():
-        print("Amo la programación")
-        func()
+print("\n")
+
+def my_decorador(func): # Decorador que recibe una función como argumento
+    def show_info(): # Función interna que extiende el comportamiento de la función original
+        print("Amo la programación") # Mensaje que se imprime antes de llamar a la función original
+        func() # Llama a la función original
         print("Mi lenguaje favorito es Python")
     return show_info
 
 
-@my_decorador
+@my_decorador # Decorador aplicado a la función enviar_greeting
 def enviar_greeting():
     print(" Me encanta programar")
     
 enviar_greeting()
+
+print("\n")
