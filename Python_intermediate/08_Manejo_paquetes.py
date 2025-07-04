de32ab22138047a1283bfa486dc822a9d4304fb1
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-07-03 22:03:26 -0400

    Update array values and adjust print statements for clarity

--- Python_intermediate/08_Manejo_paquetes.py
+++ Python_intermediate/08_Manejo_paquetes.py
@@ -3,10 +3,14 @@
 
 # Un paquete es un conjunto de módulos que se agrupan para facilitar su uso y organización.
 
+print("\n")
+
 import numpy # importación del paquete numpy
 
-array_nu = numpy.array([3, 5, 6, 8,1 ,7 ,2 ,9]) # Uso array para crear una lista de los 
+array_nu = numpy.array([3, 5, 13, 8,1 ,7 ,2 ,9]) # Uso array para crear una lista de los 
 
-print(array_nu + 4)
+print(array_nu + 6)
+
+print("\n")
 
 
