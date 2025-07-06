e0dc7d960dd54c60fde1a6fa228e3ffcf6046d2d
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-07-05 23:37:37 -0400

    Add initial content for generadores.py with generator and callback explanations

---
+++ Backend/generadores.py
@@ -0,0 +1,6 @@
+
+# Los Gneradiores
+
+# Un generador es una función que utiliza la palabra clave yield para devolver un valor y pausar su ejecución, permitiendo que se reanude más tarde desde el mismo punto. Esto es útil para generar secuencias de valores de manera eficiente, ya que no requiere almacenar todos los valores en memoria al mismo tiempo.
+
+# Tambien estudiar los callbacks
