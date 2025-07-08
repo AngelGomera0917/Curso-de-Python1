c00a05b313ff84aa40ff17aa228ebf5d5aa0aa30
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-07-08 16:51:08 -0400

    add example of class

--- Python_intermediate/05_Error_Types.py
+++ Python_intermediate/05_Error_Types.py
@@ -1,2 +1,45 @@
 
 # ========================= Error Types ========================= #
+
+class persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+        
+    def info(self):
+        print(f" Nombre: {self.nombre} ")
+        print(f" Edad: {self.edad} ")
+        
+class educacion:
+    def __init__(self, universidad):
+        self.universidad = universidad
+                
+    def info_uni(self):
+        print(f" Universidad: {self.universidad} ")
+        
+class rol:
+    def __init__(self, status):
+        self.status = status
+    
+    def info_rol(self):
+        print(f" Status: {self.status} ")
+
+class maestro(persona, educacion, rol):
+    def __init__(self, nombre, edad, universidad, status, materia ):
+        
+        persona.__init__(self, nombre, edad)
+        educacion.__init__(self, universidad)
+        rol.__init__(self, status)
+        self.materia = materia
+                
+    def info_maestro(self):
+        print(f" {self.info()} | {self.info_uni()} | {self.info_rol()} | Materia: {self.materia}")
+
+maestro_1 = maestro("Juan Martinez",30,"Intec","Docente", "Plan de Negocios")
+
+maestro_1.info_maestro()
+
+
+
+print("\n")
+
