010c079459adf8b93f60c1b76f7cbd8e34002bee
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-07-13 22:48:32 -0400

    refactor user model and endpoints for clarity and consistency

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -8,10 +8,47 @@ from pydantic import BaseModel
 # Creando una instancia de la aplicación FastAPI
 app = FastAPI()
 
-@app.get("/user")
+@app.get("/userjson")
 
 # Creamos un modelo de datos para representar un usuario
 async def user_json():
-    return [{"name": "Angel","Surname": "Gomera", "Age": 22, "Carrera_Universitaria": "Desarrollo de Software", "Url": "https://github.com/AngelGomera0917/Curso-de-Python1"}, 
-{"name": "Darli","Surname": "Diaz", "Age": 21, "Carrera_Universitaria": "Lenguas Moderna", "Url": "https://github.com/darli/Curso-de-Python1"}, 
-{"name": "Antonio","Surname": "Romero", "Age": 28, "Carrera_Universitaria": "Medicina", "Url": "https://github.com/antonio/Curso-de-Python1"} ]
+    return [{"Name": "Angel","Surname": "Gomera", "Age": 22, "Carrera_Universitaria": "Desarrollo de Software", "Url": "https://github.com/AngelGomera0917/Curso-de-Python1"}, 
+{"Name": "Darli","Surname": "Diaz", "Age": 21, "Carrera_Universitaria": "Lenguas Moderna", "Url": "https://github.com/darli/Curso-de-Python1"}, 
+{"Name": "Antonio","Surname": "Romero", "Age": 28, "Carrera_Universitaria": "Medicina", "Url": "https://github.com/antonio/Curso-de-Python1"} ]
+
+
+# Entidad Usuario
+# Definimos un modelo de datos para representar un usuario
+class Usuario(BaseModel):
+    Name: str
+    Surname: str
+    Age: int
+    Carrera_universitaria: str
+    Url: str
+
+@app.get("/user") # Creamos una ruta para retornar un usuario específico
+
+# Llamamos la clase Usuario y retornamos un objeto de tipo Usuario
+async def root_users1():
+    return Usuario(Name = "Angel", Surname = "Gomera", Age = 23, Carrera_universitaria = "Desarrollo de Software", Url = "https://github.com/AngelGomera0917/Curso-de-Python1" )
+
+
+class Usuario(BaseModel): # Definimos un modelo de datos para representar un usuario
+    Name: str
+    Surname: str 
+    Age: int
+    Carrera_universitaria: str
+    Url: str
+
+# Creamos una lista de usuarios utilizando la clase Usuario
+user_list = [Usuario(Name = "Angel", Surname = "Gomera", Age = 23, Carrera_universitaria = "Desarrollo de Software", Url = "https://github.com/AngelGomera0917/Curso-de-Python1" ),
+
+Usuario(Name = "Darli", Surname = "Diaz", Age = 23, Carrera_universitaria = "Lenguas Moderna", Url = "https://github.com/darli/Curso-de-Python1" ),
+
+Usuario(Name = "Alexander", Surname = "Romero", Age = 23, Carrera_universitaria = "Medicina", Url = "https://github.com/alexander/Curso-de-Python1" )
+
+]
+
+@app.get("/user_class")
+async def root_users2():
+    return user_list
