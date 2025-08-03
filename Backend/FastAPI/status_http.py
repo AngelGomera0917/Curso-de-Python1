e101713352d10200ed135bc8a6e1140e4a9352df
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-08-03 16:06:53 -0400

    update status

--- Backend/FastAPI/status_http.py
+++ Backend/FastAPI/status_http.py
@@ -27,34 +27,14 @@ database_veterinary = [veterinary(id = 1, raza = "Leon", stock = 5, dueño = "Ju
 async def root():
     return database_veterinary 
 
-# # Ruta del path, se utiliza para acceder a un recurso específico en la aplicación web.
-# @app.get("/veterinary_path/{id}")
-# async def root_veterinary(id: int):
-#     buscar = list(filter(lambda root_veterinary: root_veterinary.id == id, database_veterinary))
-    
-#     if buscar:
-#         return {"Mensaje": "Animal encontrado exitosamente ✅", "Type": buscar[0]}
-#     else:
-#         return {"Error": "Lo sentimos, No hemos encontrado un Animal con este ID ❌"}
-    
-    
-# # Ruta de la Query, se utiliza para enviar parámetros a la ruta para filtrar o modificar la respuesta.
-# @app.get("/veterinary_query/")
-# async def root_veterinary(id: int): 
-#     buscar = list(filter(lambda root_veterinary: root_veterinary.id == id, database_veterinary))
-    
-#     if buscar:
-#         return {"Mensaje": "Animal encontrado exitosamente ✅", "Type": buscar[0]}
-#     else:
-#         return {"Error": "Lo sentimos, No hemos encontrado un Animal con este ID ❌"}
     
 # Manejo del Post, se utiliza para enviar datos al servidor y crear un nuevo recurso.
 
-# respuesta_model es un parámetro que se utiliza para especificar el modelo de respuesta que se espera devolver al cliente. En este caso, se espera devolver un objeto de tipo veterinary, esto ayuda mas en la documentacion de Docs y la mejora.
+# response_model es un parámetro que se utiliza para especificar el modelo de respuesta que se espera devolver al cliente. En este caso, se espera devolver un objeto de tipo veterinary, esto ayuda mas en la documentacion de Docs y la mejora.
 # status_code es un parámetro que se utiliza para especificar el código de estado HTTP que
-@app.post("/veterinary_post/", response_model=veterinary, status_code=201) # status_code=201 indica que la solicitud se ha procesado correctamente y se ha creado un nuevo recurso.
+@app.post("/veterinary_post/", status_code=201, response_model=veterinary) # status_code=201 indica que la solicitud se ha procesado correctamente y se ha creado un nuevo recurso.
 
-async def root_post(animal: veterinary):
+async def root_post(animal: veterinary): 
     
     for existing_veterinary in database_veterinary:
         if existing_veterinary.id == animal.id:
@@ -65,28 +45,5 @@ async def root_post(animal: veterinary):
             raise HTTPException(status_code = 404, detail = "Ya existe un animal con esta raza ❌") # 404 indica que el recurso solicitado no se ha encontrado.
     
     database_veterinary.append(animal)
-    return {"Mensaje": "Animal agregado exitosamente ✅", "Type": animal}
+    return animal
 
-
-# # Manejo del Put, se utiliza para actualizar un recurso existente en el servidor.
-# @app.put("/veterinary_put/")
-# async def root_put(animal: veterinary):
-    
-#     for index, update_animal in enumerate(database_veterinary):
-#         if update_animal.id == animal.id:
-#             database_veterinary[index] = animal
-#             return {"Mensaje": "Animal actualizado exitosamente ✅", "Type": animal}
-        
-#     return {"Error": "Lo sentimos, No hemos encontrado un Animal con este ID ❌"} 
-
-
-# # Manejo del Delete, se utiliza para eliminar un recurso existente en el servidor.
-# @app.delete("/veterinary_delete/{id}") 
-# async def root_delete(id: int):
-    
-#     for index, delete_animal in enumerate(database_veterinary):
-#         if delete_animal.id == id:
-#             del database_veterinary[index]
-#             return {"Mensaje": "Animal eliminado exitosamente ✅"}
-        
-#     return {"Error": "Lo sentimos, No hemos encontrado un Animal con este ID ❌"}
