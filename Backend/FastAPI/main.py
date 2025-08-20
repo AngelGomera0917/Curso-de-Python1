bf00d3722ccf13b7b41b428518afb4df3a6be4e1
Angel-Gomera <20220493@itla.edu.do>
2025-08-20 14:33:13 -0400

    add comentarioal incluir la ruta de autenticacion

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -23,8 +23,8 @@ app = FastAPI()
 # Incluyendo el router de la librería
 app.include_router(libreria.router) # Incluyendo el router de usuarios
 app.include_router(users.router) # Incluyendo el router de status_http
-app.include_router(status_http.router) # incluyendo el router de status_http
-app.include_router(basic_auth_users.router) 
+app.include_router(status_http.router) # Incluyendo el router de status_http
+app.include_router(basic_auth_users.router) # Incluyendo el basic_auth_users
 
 app.mount("/static", StaticFiles(directory="static"), name="static") # Montando la carpeta 'static' para servir archivos estáticos
 
