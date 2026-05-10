121ab32d4fe8875a2baa1470926c0d0ab5dbb73e
Angel Antonio Gomera <antoniogomera12@gmail.com>
2026-05-10 10:14:43 -0400

    Add initial implementation for even and odd number classification

---
+++ Prueba.py
@@ -0,0 +1,13 @@
+edades = [14,12,4,6,7,9,5,3,17,8,11,10]
+
+guardar = []
+
+for i in edades:
+    if i % 2 == 0:
+        guardar.append(i)
+        print("Son pares: ")
+    
+    else:
+        print("Son Impares: ", guardar.append(i))
+        
+print(guardar)
